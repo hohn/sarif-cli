a0af2c8c59ce4c521ef18f7ae71769ea0154cb28
Michael Hohn <hohn@github.com>
2021-11-09 14:29:31 -0800

    fix: traverse all languages

--- sarif_cli/__init__.py
+++ sarif_cli/__init__.py
@@ -4,6 +4,10 @@ MIN_PYTHON = (3, 7)
 if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
+def indices(sarif_struct, *path):
+    """ Return a range for the indices of PATH """
+    return range(0, len(get(sarif_struct, *path)))
+
 def get(sarif_struct, *path):
     """ Get the sarif entry at PATH """
     res = sarif_struct
