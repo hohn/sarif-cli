ef0055940825f55e658d14da067585ed663405d7
Michael Hohn <hohn@github.com>
2022-07-19 15:42:26 -0700

    Bring sarif-extract-tables up to date with sarif-extract-scans

--- scripts/table-tests.sh
+++ scripts/table-tests.sh
@@ -4,5 +4,6 @@
 # nothing on stdout/stderr
 # 
 ( cd ../data/treeio/2021-12-09 && sarif-extract-tables results.sarif test-tables )
+( cd ../data/treeio/2022-02-25 && sarif-extract-tables results.sarif test-tables )
 ( cd ../data/treeio && sarif-extract-multi multi-sarif-01.json test-multi-table )
 ( cd ../data/treeio && sarif-extract-scans scan-spec-0.json test-scan )
