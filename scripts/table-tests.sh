7e996e746c9b596f516accdefdab6d3a4212efe9
Michael Hohn <hohn@github.com>
2022-08-08 14:47:25 -0700

    Rewrite sarif-runner as full tool, sarif-extract-scans-runner

--- scripts/table-tests.sh
+++ scripts/table-tests.sh
@@ -5,5 +5,26 @@
 # 
 ( cd ../data/treeio/2021-12-09 && sarif-extract-tables results.sarif test-tables )
 ( cd ../data/treeio/2022-02-25 && sarif-extract-tables results.sarif test-tables )
+
 ( cd ../data/treeio && sarif-extract-multi multi-sarif-01.json test-multi-table )
 ( cd ../data/treeio && sarif-extract-scans scan-spec-0.json test-scan )
+
+# Simple run
+( cd ../data/treeio/ &&
+      sarif-extract-scans-runner - > /dev/null <<EOF
+2021-12-09/results.sarif
+2022-02-25/results.sarif
+EOF
+)
+
+# Repeated run with state
+( cd ../data/treeio/ &&
+      sarif-extract-scans-runner -i1 -s successful-runs - <<EOF
+2021-12-09/results.sarif
+2022-02-25/results.sarif
+EOF
+  sarif-extract-scans-runner -i1 -s successful-runs - <<EOF
+2021-12-09/results.sarif
+2022-02-25/results.sarif
+EOF
+)
