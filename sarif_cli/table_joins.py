a90084826864bc43f94c30975becdea67e3b88a0
Michael Hohn <hohn@github.com>
2023-07-27 09:05:27 -0700

    Remove ipython debug call

--- sarif_cli/table_joins.py
+++ sarif_cli/table_joins.py
@@ -115,9 +115,6 @@ def joins_for_problem(tgraph, af_0350_location):
     # 
     # Form the message dataframe (@kind problem) via joins
     # 
-    import IPython
-    IPython.embed(header="spot 1")
-
     kind_problem_1 = (
         aft(6343)
         .merge(sft(4055), how="inner",
